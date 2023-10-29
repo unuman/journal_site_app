7cf9c346ffaa08e066336d7dc53562d71481817a
unuman <unumakyohey@gmail.com>
2023-10-29 20:58:52 +0900

    課題3完了

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,13 +8,14 @@
     <title>
         論文詳細
     </title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
     <h1>論文詳細</h1>
     <p>タイトル︰{{ $article->title }}</p>
     <p>{!! nl2br(e($article->body)) !!}</p>
-
+    <div class="button-group">
     <button onclick="location.href='{{ route('articles.index') }}'">一覧へ戻る</button>
     <button onclick="location.href='{{ route('articles.edit', $article) }}'">編集する</button>
     <form action="{{ route('articles.destroy', $article) }}" method="post">
@@ -22,6 +23,7 @@
         @method('DELETE')
         <input type="submit" value="削除する" onclick="if(!confirm('削除しますか？')){return false};">
     </form>
+    </div>
 </body>
 
 </html>
